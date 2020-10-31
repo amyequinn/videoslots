c3e618067c88d0d63aa57a55e5b268afc1c51e9f
Amy Quinn <amyequinn@hotmail.co.uk>
2020-10-31 12:34:33 +0000

    obtained data from form

--- src/components/image-card/image-card-component.vue
+++ src/components/image-card/image-card-component.vue
@@ -10,58 +10,36 @@ export default {
   name: "images-list-main",
   data() {
     return {
-      data: '',
-      id: '',
-      image: '',
-      author: ''
+      data: Object,
+      totalPages: '',
+      totalImages: ''
+
     }
   },
+  methods: {
+    handleClick: function(event){
+    this.totalPages = event.target.value
+    this.fetchImages()
+    },
+      fetchImages: function() {
 
-  async created() {
-      let imageElement, imageRow, imageAuthor, imageID, imageSrc, imageLink;
+          let imageTotal = 10;
 
-      let pageTotal = 2;
-      let imageTotal = 10;
-      console.log(pageTotal, imageTotal);
-      let api = `https://picsum.photos/v2/list?page=${pageTotal}l&limit=${imageTotal}`
-      await fetch(api)
-      .then(response => response.json())
+          let api = `https://picsum.photos/v2/list?page=${this.totalPages}l&limit=${imageTotal}`
+          fetch(api)
+          .then(response => response.json())
 
-      .then(data => (
-      console.log(data),
-      data.forEach(el => {
+          .then(data => (
+          this.data = data
 
-        this.id = el.id,
-        this.author = el.author,
-        this.image = el.download_url,
-        this.image = el.download_url.substring(0, this.image.length-9).concat('367/267'),
+          ));
 
-        imageElement = document.createElement('div'),
-        imageElement.className = "image-element",
-        imageRow = document.createElement('div'),
-        imageRow.className = "row",
-        imageAuthor = document.createElement('h4'),
-        imageID = document.createElement('h4'),
-        imageLink = document.createElement('a'),
-        imageLink.setAttribute('target', '_blank'),
-        imageLink.setAttribute('href', this.image),
-        imageSrc = document.createElement('img'),
-        imageSrc.setAttribute('src', this.image),
-        imageSrc.className = 'img-circle',
-        imageAuthor.innerHTML = this.author,
-        imageID.innerHTML = this.id,
-        imageLink.append(imageSrc),
-        imageRow.append(imageLink),
-        imageRow.append(imageAuthor),
-        imageRow.append(imageID),
-        imageElement.append(imageRow),
-        document.querySelector('.image-list').append(imageElement)
+      }
+  },
 
-      })
-      ));
-  }
 
 };
+
 </script>
 
 <style src="./image-card-component.css">
